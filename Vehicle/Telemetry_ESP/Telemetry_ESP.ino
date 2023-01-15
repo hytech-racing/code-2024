1e7962cd5cd5d24243dc8cac8bf9a290ca8d2693
CL16gtgh <ryang359@gatech.edu>
2023-01-15 17:16:17 -0500

    Telemetry_ESP_addGPS

--- Vehicle/Telemetry_ESP/Telemetry_ESP.ino
+++ Vehicle/Telemetry_ESP/Telemetry_ESP.ino
@@ -5,6 +5,13 @@
 #include "Metro.h"
 #include <string.h>
 
+#include <Wire.h> //Needed for I2C to GNSS
+#include <SparkFun_u-blox_GNSS_Arduino_Library.h> //http://librarymanager/All#SparkFun_u-blox_GNSS
+SFE_UBLOX_GNSS myGNSS;
+#include <XBTools.h>
+
+long lastTime = 0; //Simple local timer. Limits amount if I2C traffic to u-blox module.
+
 /* 
  * Pin Definitions
  */
@@ -17,7 +24,7 @@
 #define ESP_LED_OK 12
 #define ESP_LED_OTHER_2 13
 
-#define TCU Serial2
+#define TCU Serial1
 
 
 typedef struct rtcm_message {
@@ -86,12 +93,72 @@ void telemLoop() {
  * Sets up the GPS
  */
 void GPSSetup() {
+//  while (!Serial); //Wait for user to open terminal
+  Serial.println("SparkFun u-blox Example");
+
+  Wire.begin();
+
+  //myGNSS.enableDebugging(); // Uncomment this line to enable helpful debug messages on Serial
+
+  if (myGNSS.begin() == false) //Connect to the u-blox module using Wire port
+  {
+    Serial.println(F("u-blox GNSS not detected at default I2C address. Please check wiring. Freezing."));
+    while (1);
+  }
+
+  myGNSS.setI2COutput(COM_TYPE_UBX); //Set the I2C port to output UBX only (turn off NMEA noise)
+  myGNSS.saveConfigSelective(VAL_CFG_SUBSEC_IOPORT); //Save (only) the communications port settings to flash and BBR
+
+  // Note the format for setting a serial port is as follows: Serial2.begin(baud-rate, protocol, RX pin, TX pin);
+//  Serial.begin(115200);
+//  TCU.begin(9600, SERIAL_8N1, ESP_RX, ESP_TX);
 }
 
 /*
  * Loop to retreive data from the GPS and send correction data to the GPS
  */
 void GPSLoop() {
+  //Query module only every second. Doing it more often will just cause I2C traffic.
+  //The module only responds when a new position is available
+  if (millis() - lastTime > 1000)
+  {
+    lastTime = millis(); //Update the timer
+    
+    long latitude = myGNSS.getLatitude();
+    Serial.print(F("Lat: "));
+    Serial.print(latitude);
+
+    long longitude = myGNSS.getLongitude();
+    Serial.print(F(" Long: "));
+    Serial.print(longitude);
+    Serial.print(F(" (degrees * 10^-7)"));
+
+    long altitude = myGNSS.getAltitude();
+    Serial.print(F(" Alt: "));
+    Serial.print(altitude);
+    Serial.print(F(" (mm)"));
+
+    byte SIV = myGNSS.getSIV();
+    Serial.print(F(" SIV: "));
+    Serial.print(SIV);
+
+    Serial.println();
+
+    long lat_long[2] = {latitude, longitude};
+
+    uint8_t cobs_buf[2 + 2 * sizeof(long)];
+    cobs_encode((uint8_t *)(lat_long), 2 * sizeof(long), cobs_buf);
+
+    cobs_buf[2 * sizeof(long)+1] = 0x0;
+    for (int i = 0; i < 2 * sizeof(long)+2; i++) {
+        Serial.print(cobs_buf[i], HEX);
+        Serial.print(" ");
+    }
+    Serial.println();
+
+    //Choose TCU or Serial2 as required
+    TCU.write(cobs_buf, (size_t) (2 * sizeof(long))+2);
+  }
 }
 
 void onDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
@@ -140,6 +207,7 @@ void setup() {
   pinMode(ESP_LED_OTHER_2, OUTPUT);
 
   esp_now_setup();
+  GPSSetup();
 }
 
 void loop() {
