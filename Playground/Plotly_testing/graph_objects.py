b7797570e71f7f68b3a65e82ddac706356869c27
PKuppili <44453209+PKuppili@users.noreply.github.com>
2023-02-12 17:53:19 -0500

    Live Data and sin wave added with plotly

--- Playground/Plotly_testing/graph_objects.py
+++ Playground/Plotly_testing/graph_objects.py
@@ -1,5 +1,25 @@
 import plotly.express as px
-df = px.data.iris()
-fig = px.scatter_3d(df, x='sepal_length', y='sepal_width', z='petal_width',
-              color='species')
+import plotly.graph_objects as go
+import numpy as np
+import time
+
+
+
+#df = px.data.stocks()
+
+x = np.arange(-np.pi,np.pi,0.01)
+y = np.sin(x)
+fig = px.line(x=x,y=y, labels={'x':'x', 'y':'sin(x)'})
+fig.update_layout(title="Basic Line Plot")
 fig.show()
+
+#fig = go.Figure(data=go.Scatter(x=df["date"],y=df["GOOG"],mode="markers",name="GOOGLE Stocks"))
+#cnt = 0
+
+#while (True):
+#    time.sleep(.1)
+#    num = np.round(np.sin(2*np.pi*(cnt/100)),2)
+#    cnt = (cnt+1)%100
+    
+#    fig = px.line(df, x=cnt, y=num)
+#    fig.show()
