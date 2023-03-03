4148e20b0de325ef8262aea2b94596e7b4cdc1b2
Eric Galluzzi <ericbgalluzzi@gmail.com>
2023-03-02 20:35:12 -0500

    Update test_adc128.ino

--- Playground/Main_Control_Unit_Testing/test_adc128/test_adc128.ino
+++ Playground/Main_Control_Unit_Testing/test_adc128/test_adc128.ino
@@ -33,28 +33,75 @@ int readADC(int CS, int chan) {
   
   return (hi << 8) | lo; 
 }
+void readADCTemp(int CS) {
+  digitalWrite(CS, LOW);
+  SPI.beginTransaction(SPISettings(ADC_SPI_SPEED, MSBFIRST, SPI_MODE0));
+  SPI.transfer( ADC_RR_LOAD_CELL_CHANNEL << 3);
+  int hi1 = SPI.transfer( 0 ) ;
+  int lo1 = SPI.transfer( 0 );
+  int hilo1 = (hi1 << 8) | lo1;
+//
+//  int hi2 = SPI.transfer( ADC_FL_LOAD_CELL_CHANNEL << 3);
+//  int lo2 = SPI.transfer( 0 );
+//  int hilo2 = (hi2 << 8) | lo2;
+////
+  SPI.transfer( ADC_FR_LOAD_CELL_CHANNEL << 3);
+  int hi3 = SPI.transfer( 0 ) ;
+  int lo3 = SPI.transfer( 0 );
+  int hilo3 = (hi3 << 8) | lo3;
+  
+  
+  digitalWrite(CS, HIGH);
+  SPI.endTransaction();
+
+  int RR = hilo1;
+//  int FL = hilo2;
+//  int FR = hilo3;
+  
+  
+//      Serial.print("FL_LOAD_CELL: ");
+//      Serial.println(FL);
+//      Serial.print("FR_LOAD_CELL: ");
+//      Serial.println(FR);
+      Serial.print(" RR_LOAD_CELL: ");
+      Serial.println(RR);
+ 
+}
 
 void adc_sweep() {
   if(timer_load_cells_read.check()) {
+//    int RR = readADC(ADC2_CS, ADC_RR_LOAD_CELL_CHANNEL);
+
+      int FL = readADC(ADC2_CS, ADC_FL_LOAD_CELL_CHANNEL);
+
+//      int FR = 0;
       int FR = readADC(ADC2_CS, ADC_FR_LOAD_CELL_CHANNEL);
-//      int FL = readADC(ADC2_CS, ADC_FL_LOAD_CELL_CHANNEL);
-// int RR = readADC(ADC2_CS, ADC_RR_LOAD_CELL_CHANNEL);
- //     int RL = readADC(ADC1_CS, ADC_RL_LOAD_CELL_CHANNEL);
+
+      
+      
+      int RR = readADC(ADC2_CS, ADC_RR_LOAD_CELL_CHANNEL);
+      delay(10);
 //
-      Serial.print("FR_LOAD_CELL: ");
-      Serial.println(FR);
-//      Serial.print("FL_LOAD_CELL: ");
-//      Serial.println(FL);
-//      Serial.print("RR_LOAD_CELL: ");
-////      Serial.println(RR);
+
+      Serial.print("FL_LOAD_CELL: ");
+      Serial.println(FL);
+//       Serial.print("FR_LOAD_CELL: ");
+//      Serial.println(FR);
+      Serial.print("RR_LOAD_CELL: ");
+      Serial.println(RR);
 //      Serial.print("RL_LOAD_CELL: ");
 //      Serial.println(RL);
 //      
   }
 }
+void adc_sweepTemp() {
+  if(timer_load_cells_read.check()) {
+    readADCTemp(ADC2_CS);
+  }
+}
 void loop() {
   // put your main code here, to run repeatedly:
-  adc_sweep();
+  adc_sweepTemp();
 
 
 }
