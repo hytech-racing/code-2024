e63aee95c879d145b56b2a30531f356d839ebd6d
Andrew <atzhang@live.com>
2023-04-11 01:52:03 -0400

    parsing messages and reversing endianness

---
+++ Telemetry/telemetry_parsers/parsers.py
@@ -0,0 +1,422 @@
+import pandas as pd
+import numpy as np
+
+def parse_BMS_balancing_status(ordering, id, data):
+    msg_id = 0xDE
+    mask = id==msg_id
+    data = data[mask]
+    
+
+
+    #df = pd.DataFrame(arr, columns='cols')
+    return 
+
+def parse_BMS_coulomb_counts(ordering, id, data):
+    msg_id = 0xE2
+    mask = id==msg_id
+    data = data[mask]
+    
+
+
+    #df = pd.DataFrame(arr, columns='cols')
+    return 
+
+def parse_BMS_detailed_temperatures(ordering, id, data):
+    msg_id = 0xDA
+    mask = id==msg_id
+    data = data[mask]
+    
+
+    out = np.zeros((data.size, 5))
+    out[:,0] = data & 0xF
+    out[:,1] = (data & (0xF<<4))>>4
+    out[:,2] = (data & (0xFFFF<<8))>>8
+    out[:,3] = (data & (0xFFFF<<24))>>24
+    out[:,4] = (data & (0xFFFF<<40))>>40
+
+    #df = pd.DataFrame(arr, columns='cols')
+    return out
+
+def parse_BMS_detailed_voltages(ordering, id, data):
+    msg_id = 0xD8
+    mask = id==msg_id
+    data = data[mask]
+    
+
+
+    #df = pd.DataFrame(arr, columns='cols')
+    return 
+
+def parse_BMS_onboard_detailed_temperatures(ordering, id, data):
+    msg_id = 0xD6
+    mask = id==msg_id
+    data = data[mask]
+    
+
+
+    #df = pd.DataFrame(arr, columns='cols')
+    return 
+
+def parse_BMS_onboard_temperatures(ordering, id, data):
+    msg_id = 0xD5
+    mask = id==msg_id
+    data = data[mask]
+    
+
+
+    #df = pd.DataFrame(arr, columns='cols')
+    return 
+
+def parse_BMS_status(ordering, id, data):
+    msg_id = 0xDB
+    mask = id==msg_id
+    data = data[mask]
+    
+
+
+    #df = pd.DataFrame(arr, columns='cols')
+    return 
+
+def parse_BMS_temperatures(ordering, id, data):
+    msg_id = 0xD9
+    mask = id==msg_id
+    data = data[mask]
+    
+
+
+    #df = pd.DataFrame(arr, columns='cols')
+    return 
+
+def parse_BMS_voltages(ordering, id, data):
+    msg_id = 0xD7
+    mask = id==msg_id
+    data = data[mask]
+    
+
+
+    #df = pd.DataFrame(arr, columns='cols')
+    return 
+
+def parse_CCU_status(ordering, id, data):
+    msg_id = 0xDD
+    mask = id==msg_id
+    data = data[mask]
+    
+
+
+    #df = pd.DataFrame(arr, columns='cols')
+    return 
+
+def parse_Charger_configure(ordering, id, data):
+    msg_id = 0x1806E0F4
+    mask = id==msg_id
+    data = data[mask]
+    
+
+
+    #df = pd.DataFrame(arr, columns='cols')
+    return 
+
+def parse_Charger_data(ordering, id, data):
+    msg_id = 0x18FF50E0
+    mask = id==msg_id
+    data = data[mask]
+    
+
+
+    #df = pd.DataFrame(arr, columns='cols')
+    return 
+
+def parse_Dashboard_status(ordering, id, data):
+    msg_id = 0xEB
+    mask = id==msg_id
+    data = data[mask]
+    
+
+
+    #df = pd.DataFrame(arr, columns='cols')
+    return 
+
+def parse_MCU_GPS_readings(ordering, id, data):
+    msg_id = 0xE7
+    mask = id==msg_id
+    data = data[mask]
+    
+
+
+    #df = pd.DataFrame(arr, columns='cols')
+    return 
+
+def parse_MCU_pedal_readings(ordering, id, data):
+    msg_id = 0xC4
+    mask = id==msg_id
+    data = data[mask]
+    
+
+
+    #df = pd.DataFrame(arr, columns='cols')
+    return 
+
+def parse_MCU_status(ordering, id, data):
+    msg_id = 0xC3
+    mask = id==msg_id
+    data = data[mask]
+    
+
+
+    #df = pd.DataFrame(arr, columns='cols')
+    return 
+
+def parse_MCU_analog_readings(ordering, id, data):
+    msg_id = 0xCC
+    mask = id==msg_id
+    data = data[mask]
+    
+
+
+    #df = pd.DataFrame(arr, columns='cols')
+    return 
+
+def parse_MCU_load_cells(ordering, id, data):
+    msg_id = 0xC5
+    mask = id==msg_id
+    data = data[mask]
+    
+    arr = np.zeros((data.size, 4))
+    arr[:,0] = (data & 0xFFFF)
+    arr[:,1] = (data & 0xFFFF<<16)>>48
+    arr[:,2] = (data & 0xFFFF<<32)>>48
+    arr[:,3] = (data & 0xFFFF<<48)>>48
+
+    #df = pd.DataFrame(arr, columns='cols')
+    return arr
+
+def parse_MC1_energy(ordering, id, data):
+    msg_id = 0xA8
+    mask = id==msg_id
+    data = data[mask]
+    
+
+
+    #df = pd.DataFrame(arr, columns='cols')
+    return 
+
+def parse_MC2_energy(ordering, id, data):
+    msg_id = 0xA9
+    mask = id==msg_id
+    data = data[mask]
+    
+
+
+    #df = pd.DataFrame(arr, columns='cols')
+    return 
+
+def parse_MC3_energy(ordering, id, data):
+    msg_id = 0xAA
+    mask = id==msg_id
+    data = data[mask]
+    
+
+
+    #df = pd.DataFrame(arr, columns='cols')
+    return 
+
+def parse_MC4_energy(ordering, id, data):
+    msg_id = 0xAB
+    mask = id==msg_id
+    data = data[mask]
+    
+
+
+    #df = pd.DataFrame(arr, columns='cols')
+    return 
+
+def parse_MC1_setpoints_command(ordering, id, data):
+    msg_id = 0xB0
+    mask = id==msg_id
+    data = data[mask]
+    
+
+
+    #df = pd.DataFrame(arr, columns='cols')
+    return 
+
+def parse_MC2_setpoints_command(ordering, id, data):
+    msg_id = 0xB1
+    mask = id==msg_id
+    data = data[mask]
+    
+
+
+    #df = pd.DataFrame(arr, columns='cols')
+    return 
+
+def parse_MC3_setpoints_command(ordering, id, data):
+    msg_id = 0xB2
+    mask = id==msg_id
+    data = data[mask]
+    
+
+
+    #df = pd.DataFrame(arr, columns='cols')
+    return 
+
+def parse_MC4_setpoints_command(ordering, id, data):
+    msg_id = 0xB3
+    mask = id==msg_id
+    data = data[mask]
+    
+
+
+    #df = pd.DataFrame(arr, columns='cols')
+    return 
+
+def parse_MC1_status(ordering, id, data):
+    msg_id = 0xA0
+    mask = id==msg_id
+    data = data[mask]
+    
+
+
+    #df = pd.DataFrame(arr, columns='cols')
+    return 
+
+def parse_MC2_status(ordering, id, data):
+    msg_id = 0xA1
+    mask = id==msg_id
+    data = data[mask]
+    
+
+
+    #df = pd.DataFrame(arr, columns='cols')
+    return 
+
+def parse_MC3_status(ordering, id, data):
+    msg_id = 0xA2
+    mask = id==msg_id
+    data = data[mask]
+    
+
+
+    #df = pd.DataFrame(arr, columns='cols')
+    return 
+
+def parse_MC4_status(ordering, id, data):
+    msg_id = 0xA3
+    mask = id==msg_id
+    data = data[mask]
+    
+
+
+    #df = pd.DataFrame(arr, columns='cols')
+    return 
+
+def parse_MC1_temps(ordering, id, data):
+    msg_id = 0xA4
+    mask = id==msg_id
+    data = data[mask]
+    
+
+
+    #df = pd.DataFrame(arr, columns='cols')
+    return 
+
+def parse_MC2_temps(ordering, id, data):
+    msg_id = 0xA5
+    mask = id==msg_id
+    data = data[mask]
+    
+
+
+    #df = pd.DataFrame(arr, columns='cols')
+    return 
+
+def parse_MC3_temps(ordering, id, data):
+    msg_id = 0xA6
+    mask = id==msg_id
+    data = data[mask]
+    
+
+
+    #df = pd.DataFrame(arr, columns='cols')
+    return 
+
+def parse_MC4_temps(ordering, id, data):
+    msg_id = 0xA7
+    mask = id==msg_id
+    data = data[mask]
+    
+
+
+    #df = pd.DataFrame(arr, columns='cols')
+    return 
+
+def parse_SAB_readings_front(ordering, id, data):
+    msg_id = 0x92
+    mask = id==msg_id
+    data = data[mask]
+    
+
+
+    #df = pd.DataFrame(arr, columns='cols')
+    return 
+
+def parse_SAB_readings_rear(ordering, id, data):
+    msg_id = 0x93
+    mask = id==msg_id
+    data = data[mask]
+    
+
+
+    #df = pd.DataFrame(arr, columns='cols')
+    return 
+
+def parse_SAB_readings_gps(ordering, id, data):
+    msg_id = 0xEE
+    mask = id==msg_id
+    data = data[mask]
+    
+
+
+    #df = pd.DataFrame(arr, columns='cols')
+    return 
+
+def parse_EM_status(ordering, id, data):
+    msg_id = 0x100
+    mask = id==msg_id
+    data = data[mask]
+    
+
+
+    #df = pd.DataFrame(arr, columns='cols')
+    return 
+
+def parse_EM_measurement(ordering, id, data):
+    msg_id = 0x400
+    mask = id==msg_id
+    data = data[mask]
+    
+
+
+    #df = pd.DataFrame(arr, columns='cols')
+    return 
+
+def parse_IMU_accelerometer(ordering, id, data):
+    msg_id = 0x90
+    mask = id==msg_id
+    data = data[mask]
+    
+
+
+    #df = pd.DataFrame(arr, columns='cols')
+    return 
+
+def parse_IMU_gryoscope(ordering, id, data):
+    msg_id = 0x91
+    mask = id==msg_id
+    data = data[mask]
+    
+
+
+    #df = pd.DataFrame(arr, columns='cols')
+    return 
--- Telemetry/telemetry_parsers/vector_parser_api.py
+++ Telemetry/telemetry_parsers/vector_parser_api.py
@@ -1,7 +1,35 @@
-import os, sys
+import os
+import sys
 import pandas as pd
 import numpy as np
 from datetime import datetime
+
+def parse_folder():
+    '''
+    @brief: Locates Raw_Data directory or else throws errors. Created Parsed_Data directory if not created.
+            Calls the parse_file() function on each raw CSV and alerts the user of parsing progress.
+    @input: N/A
+    @return: N/A
+    '''
+
+    # Stop attempting to parse if Raw_Data is not there.
+    if not os.path.exists("Raw_Data"):
+        print("FATAL ERROR: Raw_Data folder does not exist. Please move parser.py or create Raw_Data folder.")
+        sys.exit(0)
+
+    # Creates Parsed_Data folder if not there.
+    if not os.path.exists("Parsed_Data"):
+        os.makedirs("Parsed_Data")
+
+    # Loops through files and call parse_file on each raw CSV.
+    for file in os.listdir("Raw_Data"):
+        filename = os.fsdecode(file)
+        if filename.endswith(".CSV") or filename.endswith(".csv"):
+            parse_file(filename)
+            print("Successfully parsed: " + filename)
+        else:
+            continue
+    return 
 
 def parse_file(filename):
     '''
@@ -15,15 +43,17 @@ def parse_file(filename):
     infile_name = "Raw_Data/" + filename
     outfile_name = "Parsed_Data/" + filename
 
-    df = pd.read_csv(infile_name, on_bad_lines = "skip")
-    df = df[df["time"].str.len()==13]
-    df = df[df['msg.len'].str.len()==1]
+    #Cleans up data
+    df = pd.read_csv(infile_name, on_bad_lines="skip")
+    df = df[df["time"].str.len() == 13]
+    df = df[df['msg.len'].str.len() == 1]
     df = df[~df['msg.id'].isnull()]
-    df = df[~(df['msg.id'].str.len()<2)]
-    df = df[~(df['msg.id'].str.len()>3)]
-    df["msg.id"] = df["msg.id"].apply(int, base = 16)
+    df = df[~(df['msg.id'].str.len() < 2)]
+    df = df[~(df['msg.id'].str.len() > 3)]
+    df["msg.id"] = df["msg.id"].apply(int, base=16)
     df["msg.len"] = pd.to_numeric(df['msg.len'])
     df = df[df['msg.len']*2 == df['data'].str.len()]
     df['time'] = pd.to_datetime(df['time'], unit='ms')
 
-
+def parse_message_vectorized(id, data):
+    return
