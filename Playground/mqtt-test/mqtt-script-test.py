86e9b018587a3b37f3834ff0440295b8c2f78911
PKuppili <44453209+PKuppili@users.noreply.github.com>
2023-04-21 00:08:02 -0400

    update test

--- Playground/mqtt-test/mqtt-script-test.py
+++ Playground/mqtt-test/mqtt-script-test.py
@@ -7,9 +7,9 @@ from random import randrange, uniform
 import numpy as np
 import time
 
-mqttBroker ="hytech-virtual-machine" 
+mqttBroker = "test.mosquitto.org"    #"hytech-virtual-machine" 
 
-client = mqtt.Client("Temperature_Inside")
+client = mqtt.Client("I dont think it really matters")
 client.connect(mqttBroker)
 cnt = 0
 
@@ -18,6 +18,11 @@ while True:
     randNumber = np.round(np.sin(2*np.pi*(cnt/100)),2) #uniform(20.0, 21.0)
     cnt = (cnt+1)%100
     print(randNumber)
-    client.publish("TEMPERATURE", randNumber)
-    print("Just published " + str(randNumber) + " to topic TEMPERATURE")
+    client.publish("BMS/detailed_voltage/ic_00", randNumber)
+    client.publish("BMS/detailed_voltage/ic_02", randNumber + 1)
+    client.publish("BMS/detailed_voltage/ic_04", randNumber + 2)
+    client.publish("BMS/detailed_voltage/ic_06", randNumber + 3)
+    client.publish("BMS/detailed_voltage/ic_08", randNumber - 1)
+    client.publish("BMS/detailed_voltage/ic_10", randNumber - 2)
+    print("Just published " + str(randNumber) + " to topic")
     time.sleep(.1)
