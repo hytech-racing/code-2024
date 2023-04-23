2c6c425911d28f792c6ff1efa8c8a7410ea786cf
PKuppili <44453209+PKuppili@users.noreply.github.com>
2023-04-23 17:49:12 -0400

    Completed rough draft of the script

--- Telemetry/telemetry_parsers/mqtt.py
+++ Telemetry/telemetry_parsers/mqtt.py
@@ -1,20 +1,22 @@
 from parsers import MESSAGE_DICT
 import paho.mqtt.client as mqtt
+import struct
 
 MQTT_SERVER = ""
 MQTT_PORT   = 1883
-MQTT_TOPIC  = 'hytech_car/telemetry'
+MQTT_TOPIC  = '+/can_message'
+client = mqtt.Client()
 
-def on_connect(client, userdata, flags, rc, topic):
+
+def on_connect(client, userdata, flags, rc):
     print("Connected with result code: " + str(rc))
 
-    client.subscribe(topic)
+    client.subscribe(MQTT_TOPIC)
 
 def on_message(client, userdata, msg):
     print(msg.topic + " " + str(msg.payload))
 
 def initialize_mqtt():
-    client = mqtt.Client()
     client.on_connect = on_connect
     client.on_message = on_message
     client.connect(MQTT_SERVER, MQTT_PORT, 60)
@@ -23,9 +25,12 @@ def initialize_mqtt():
 def upload_parsed_data(list):
     for i in list:
         directory = i[3]
-        for i in len(directory):
-            topic = directory[i].replace(".", "/")
-            publish_data(topic)
+        for j in len(directory):
+            topic = directory[j].replace(".", "/")
+            payload = i[0][j]
+            payload = struct.pack('<f', payload)
 
-def publish_data(topic, client):
-    
+            publish_data(topic, payload)
+
+def publish_data(topic, payload):
+    client.publish(topic, payload)
