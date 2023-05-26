9899645e3c33fdcf9a8dea2f9bf4d9b98ab78d0c
Andrew <atzhang@live.com>
2023-05-26 13:20:55 -0400

    process multiple messages at once

--- Telemetry/telemetry_parsers/mqtt.py
+++ Telemetry/telemetry_parsers/mqtt.py
@@ -3,9 +3,9 @@ import paho.mqtt.client as mqtt
 import struct
 import numpy as np
 
-MQTT_SERVER = ""
+MQTT_SERVER = "3.134.2.166"
 MQTT_PORT   = 1883
-MQTT_TOPIC  = '+/can_message'
+MQTT_TOPIC  = 'HT07/CAN'
 client = mqtt.Client()
 
 
@@ -15,12 +15,14 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe(MQTT_TOPIC)
 
 def on_message(client, userdata, msg):
-    print(msg.topic + " " + str(msg.payload))
-    message = msg.payload
-    msg_list = message.split(",")
-
-    l = MESSAGE_DICT[int(msg_list[1], base=16)][0] (np.uint64(int(msg_list[3], base=16)))
-    upload_parsed_data(l)
+    #print(msg.topic + " " + str(msg.payload))
+    message = msg.payload.decode('ascii').rstrip('\x00')
+    msg_list = str(message).split("\n")
+    for i in range(len(msg_list)-1):
+        msg_split = msg_list[i].split(",")
+        l = MESSAGE_DICT[int(msg_split[1], base=16)][0](np.uint64(int(msg_split[3], base=16)).byteswap()>>np.uint16(8*(8-int(msg_split[2]))))
+        #print(l)
+        upload_parsed_data(l)
 
 
 def initialize_mqtt():
@@ -32,12 +34,15 @@ def initialize_mqtt():
 def upload_parsed_data(data_list):
     for i in data_list:
         directory = i[3]
-        for j in len(directory):
+        for j in range(len(directory)):
             topic = directory[j].replace(".", "/")
-            payload = i[0][j]
+            payload = i[0].flatten()[j]
+            #print(payload)
             payload = struct.pack('<f', payload)
 
             publish_data(topic, payload)
 
 def publish_data(topic, payload):
     client.publish(topic, payload)
+
+initialize_mqtt()
