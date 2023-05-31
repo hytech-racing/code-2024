09e0e04a135104c6cf8098f7c408dae449f2fce4
Andrew <atzhang@live.com>
2023-05-31 18:18:02 -0400

    more generalized plots

--- Telemetry/telemetry_parsers/plotlydashboard.py
+++ Telemetry/telemetry_parsers/plotlydashboard.py
@@ -11,20 +11,23 @@ import re
 
 MQTT_SERVER = "3.134.2.166"
 MQTT_PORT   = 1883
-MQTT_TOPIC  = 'BMS/detailed_voltage/ic_10'
+MQTT_TOPIC_LIST  = ['BMS/detailed_voltage/#']
 client = mqtt.Client()
 
 datadict = {}
 
+a = 0
+
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code: " + str(rc))
-    client.subscribe(MQTT_TOPIC)
+    for topic in MQTT_TOPIC_LIST:
+        client.subscribe(topic)
 
 def on_message(client, userdata, msg):
     if msg.topic in datadict.keys():
-        datadict[msg.topic] = np.append(datadict[msg.topic], [[time.time(), struct.unpack("<f",msg.payload)]], axis = 0)
+        datadict[msg.topic] = np.append(datadict[msg.topic], [[time.time(), struct.unpack("<f",msg.payload)[0]]], axis = 0)
     else:
-        datadict[msg.topic] = np.array([[time.time(), struct.unpack("<f",msg.payload)]])
+        datadict[msg.topic] = np.array([[time.time(), struct.unpack("<f",msg.payload)[0]]])
 
 def initialize_mqtt():
     client.on_connect = on_connect
@@ -32,27 +35,60 @@ def initialize_mqtt():
     client.connect(MQTT_SERVER, MQTT_PORT, 60)
     client.loop_start()
 
-app = Dash(__name__)
-
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.BOOTSTRAP])
 
 app.layout = dbc.Container([
+    html.Div(id="hidden-div", style={"display":"none"}),
+    html.H1("HyTech Racing Live Telemetry", className="display-3"),
+    html.Hr(className="my-2"),
+    #dbc.Button("Clear Data", id = 'data_clear_button', color="primary", className="me-1"),
     dcc.Graph(
-        id='live-update-graph'),
+        id='detailed_voltages_graph'),
+    dcc.Graph(
+        id='detailed_temps_graph'),
     dcc.Interval(
             id='interval-component',
-            interval=500, # in milliseconds
+            interval=1000, # in milliseconds
             n_intervals=0
         )
 ])
 
+#@app.callback(
+#        Input("data_clear_button", "n_clicks")
+#)
+def clear_data(n):
+    datadict = {}
+
 @app.callback(
-        Output('live-update-graph', 'figure'),
+        Output('detailed_temps_graph', 'figure'),
         Input('interval-component', 'n_intervals')
     )
-def update_graph_live(n):
+def detailed_temps_graph(n):
 
+    keys = get_keys_matching_regex(datadict, "BMS\/detailed_temperatures\/ic_\d*\/cell_\d*")
+    data = create_scatter_plots(keys)
+    return {'data': data,
+            'layout': {
+            'title': 'Detailed Temps'
+        }}
+    '''
+    return {'data': [data],
+            'layout' : go.Layout(xaxis=dict(
+                    range=[min(X),max(X)]),yaxis = 
+                    dict(range = [min(l),max(l)]),
+                    )}
+    '''
+
+@app.callback(
+        Output('detailed_voltages_graph', 'figure'),
+        Input('interval-component', 'n_intervals')
+    )
+def update_detailed_voltages_graph_live(n):
+    #global a
     keys = get_keys_matching_regex(datadict, "BMS\/detailed_voltage\/ic_\d*\/cell_\d*")
     data = create_scatter_plots(keys)
+    #a = a+1
+    #print(a)
     
     return {'data': data,
             'layout': {
@@ -76,7 +112,7 @@ def create_scatter_plots(topics):
     for t in topics:
         graphs.append(
             go.Scatter(
-                x = datadict[t][:,0],
+                x = datadict[t][:,0].astype('datetime64[s]'),
                 y = datadict[t][:,1],
                 name = t,
                 mode= 'lines'
