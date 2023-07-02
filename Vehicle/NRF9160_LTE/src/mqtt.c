9bbfe0108350500498d1a278a0ba20d54dbca385
Andrew <atzhang@live.com>
2023-07-02 13:25:36 -0400

    update lte modem

--- Vehicle/NRF9160_LTE/src/mqtt.c
+++ Vehicle/NRF9160_LTE/src/mqtt.c
@@ -660,17 +660,23 @@ do_connect:
 #define UART_DEVICE_NODE DT_CHOSEN(zephyr_shell_uart)
 //#define UART_DEVICE_NODE DT_NODELABEL(uart1)
 
-#define MSG_SIZE 64
+#define MSG_SIZE 1024
+
+typedef struct uart_message {
+	uint32_t length;
+	char buf[MSG_SIZE];
+} uart_message;
 
 /* queue to store up to 64 messages (aligned to 4-byte boundary) */
-K_MSGQ_DEFINE(uart_msgq, MSG_SIZE, 128, 4);
+K_MSGQ_DEFINE(uart_msgq, sizeof(uart_message), 64, 4);
 
 static const struct device *uart_dev = DEVICE_DT_GET(UART_DEVICE_NODE);
 static const struct device *gpio = DEVICE_DT_GET(DT_NODELABEL(gpio0));
 
 /* receive buffer used in UART ISR callback */
-static char rx_buf[MSG_SIZE];
-static int rx_buf_pos;
+//static char rx_buf[MSG_SIZE];
+//static int rx_buf_pos;
+static uart_message rx_buf;
 extern struct mqtt_client client;
 
 /*
@@ -680,6 +686,7 @@ extern struct mqtt_client client;
 void serial_cb(const struct device *dev, void *user_data)
 {
 	uint8_t c;
+	static uint8_t newlines = 0;
 
 	if (!uart_irq_update(uart_dev)) {
 		return;
@@ -689,20 +696,29 @@ void serial_cb(const struct device *dev, void *user_data)
 
 		uart_fifo_read(uart_dev, &c, 1);
 
-		if ((c == '\n' || c == '\r') && rx_buf_pos > 0) {
-			/* terminate string */
-			rx_buf[rx_buf_pos] = '\0';
+		//if ((c == '\n' || c == '\r') && rx_buf_pos > 0) {
+		if ((c == (uint8_t) 0) && rx_buf.length > 0) {
+			newlines++;
+			if (newlines >= 2)
+			{
+				/* terminate string */
+				rx_buf.buf[rx_buf.length] = '\0';
 
-			//data_publish(&client, MQTT_QOS_1_AT_LEAST_ONCE,
-			//	rx_buf, strlen(rx_buf) + 1);
+				//data_publish(&client, MQTT_QOS_1_AT_LEAST_ONCE,
+				//	rx_buf, strlen(rx_buf) + 1);
 
-			/* if queue is full, message is silently dropped */
-			k_msgq_put(&uart_msgq, &rx_buf, K_NO_WAIT);
+				/* if queue is full, message is silently dropped */
+				k_msgq_put(&uart_msgq, &rx_buf, K_NO_WAIT);
 
-			/* reset the buffer (it was copied to the msgq) */
-			rx_buf_pos = 0;
-		} else if (rx_buf_pos < (sizeof(rx_buf) - 1)) {
-			rx_buf[rx_buf_pos++] = c;
+				/* reset the buffer (it was copied to the msgq) */
+				rx_buf.length = 0;
+				newlines = 0;
+			} else {
+				rx_buf.buf[rx_buf.length++] = c;
+			}
+		} else if (rx_buf.length < (sizeof(rx_buf) - 1)) {
+			newlines = 0;
+			rx_buf.buf[rx_buf.length++] = c;
 		}
 		/* else: characters beyond buffer size are dropped */
 	}
@@ -728,7 +744,7 @@ void uart_thread(void) {
 		.flow_ctrl = UART_CFG_FLOW_CTRL_NONE
 		};
 	uart_configure(uart_dev, &cfg);
-	char tx_buf[MSG_SIZE];
+	uart_message tx_buf;
 
 	if (!device_is_ready(uart_dev)) {
 		LOG_INF("UART device not found!");
@@ -751,8 +767,8 @@ void uart_thread(void) {
 
 	while (1) {
 		if (k_msgq_get(&uart_msgq, &tx_buf, K_FOREVER) == 0) {
-			print_uart("Echo: ");
-			print_uart(tx_buf);
+			print_uart("Echo: Hello");
+			//print_uart(tx_buf.buf);
 			print_uart("\r\n");
 			
 			int ret;
@@ -761,11 +777,11 @@ void uart_thread(void) {
 					MQTT_QOS_1_AT_LEAST_ONCE,
 					CONFIG_BUTTON_EVENT_PUBLISH_MSG,
 					sizeof(CONFIG_BUTTON_EVENT_PUBLISH_MSG));*/
-			
 			ret = data_publish(&client,
 					MQTT_QOS_1_AT_LEAST_ONCE,
-					tx_buf,
-					strlen(tx_buf)+1);
+					tx_buf.buf,
+					tx_buf.length);
+			print_uart("After Publish\n");
 			if (ret) {
 				LOG_ERR("Publish failed: %d", ret);
 			}
