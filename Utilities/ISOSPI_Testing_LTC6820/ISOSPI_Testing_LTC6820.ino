37e2a3150ce6de30ab2217ac3037de23f68f5b6b
shaynoorani <113149316+shaynoorani@users.noreply.github.com>
2023-08-10 23:56:17 -0400

    test bias

--- Utilities/ISOSPI_Testing_LTC6820/ISOSPI_Testing_LTC6820.ino
+++ Utilities/ISOSPI_Testing_LTC6820/ISOSPI_Testing_LTC6820.ino
@@ -3,7 +3,7 @@
 
 #define SPI_CS    	10 		   // SPI slave select
 #define ADC_VREF    3300     // 3.3V Vref
-#define ADC_CLK     500000  // SPI clock 1.6MHz
+#define ADC_CLK     1000000  // SPI clock 1.6MHz
 
 MCP3204 adc(ADC_VREF, SPI_CS);
 
@@ -12,31 +12,31 @@ void setup() {
   pinMode(SPI_CS, OUTPUT);
 
   // set initial PIN state
-  digitalWrite(SPI_CS, HIGH);
+  digitalWrite(SPI_CS, LOW);
 
   // initialize serial
   Serial.begin(115200);
 
   // initialize SPI interface for MCP3208
-  // SPISettings settings(ADC_CLK, MSBFIRST, SPI_MODE0);
+  SPISettings settings(ADC_CLK, MSBFIRST, SPI_MODE0);
   SPI.begin();
-  // SPI.beginTransaction(settings);
+  SPI.beginTransaction(settings);
   
 }
 
 void loop() {
-  // uint32_t t1;
-  // uint32_t t2;
+  uint32_t t1;
+  uint32_t t2;
 
-  // // start sampling
-  // Serial.println("Reading...");
+  // start sampling
+  Serial.println("Reading...");
 
-  // t1 = micros();
-  // uint16_t raw = adc.read(MCP3204::Channel::SINGLE_0);
-  // t2 = micros();
+  t1 = micros();
+  uint16_t raw = adc.read(MCP3204::Channel::SINGLE_0);
+  t2 = micros();
 
-  // // get analog value
-  // uint16_t val = adc.toAnalog(raw);
+  // get analog value
+  uint16_t val = adc.toAnalog(raw);
 
   // // readed value
   // Serial.print("value: ");
@@ -52,35 +52,39 @@ void loop() {
 
   // delay(2000);
 
-  SPISettings settings(ADC_CLK, MSBFIRST, SPI_MODE0);
+  // SPISettings settings(ADC_CLK, MSBFIRST, SPI_MODE0);
 
-  SPI.beginTransaction(settings);
+  // SPI.beginTransaction(settings);
 
-  // activate ADC with chip select
-  digitalWrite(SPI_CS, LOW);
+  // // activate ADC with chip select
+  // digitalWrite(SPI_CS, LOW);
 
-  delayMicroseconds(1);
+  // delayMicroseconds(1);
 
-  digitalWrite(SPI_CS, HIGH);
+  // // digitalWrite(SPI_CS, HIGH);
 
-  delayMicroseconds(16);
+  // delayMicroseconds(16);
 
-  digitalWrite(SPI_CS, LOW);
+  // digitalWrite(SPI_CS, LOW);
 
-  // // send first command byte
-  // mSpi->transfer(cmd.hiByte);
-  // // send second command byte and receive first(msb) 4 bits
-  // adc.hiByte = mSpi->transfer(cmd.loByte) & 0x0F;
-  // // receive last(lsb) 8 bits
-  // adc.loByte = mSpi->transfer(0x00);
+  // // // send first command byte
+  // // mSpi->transfer(cmd.hiByte);
+  // // // send second command byte and receive first(msb) 4 bits
+  // // adc.hiByte = mSpi->transfer(cmd.loByte) & 0x0F;
+  // // // receive last(lsb) 8 bits
+  // // adc.loByte = mSpi->transfer(0x00);
 
-  delayMicroseconds(1);
+  // delayMicroseconds(1);
 
-  // deactivate ADC with slave select
-  digitalWrite(SPI_CS, HIGH);
+  // // deactivate ADC with slave select
+  // digitalWrite(SPI_CS, HIGH);
 
-  delayMicroseconds(1);
+  // delayMicroseconds(1);
 
-  SPI.endTransaction();
+  // SPI.endTransaction();
+
+  // Serial.println("I've uploaded code");
+
+  // delay(1000);
 
 }
