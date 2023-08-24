a4085eac9247071d43e869faea69bc654f4f6902
PKuppili <44453209+PKuppili@users.noreply.github.com>
2023-08-24 19:07:50 -0400

    More classes added

--- Telemetry/telemetry_parsers/data.py
+++ Telemetry/telemetry_parsers/data.py
@@ -49,3 +49,83 @@ class data(ABC):
         int_vals = np.array(np.int64(uint_vals & np.uint64(mask))).reshape((-1,))
         int_vals[int_vals >= (1 << (bit_width - 1))] -= (1 << bit_width)
         return int_vals
+    
+
+class BMS_balancing_status(data):
+    def parse(data, id = None, time = None):
+        msg_id = 0xDE
+        if id is not None:
+            mask = id==msg_id
+            data = data[mask]
+            if time is not None:
+                time = time[mask]
+        bitoffsets, bitmasks = get_offsets_masks(vectors)
+        arr = parse_to_np(data, vectors, bitoffsets, bitmasks, time = time)
+
+        #df = pd.DataFrame(arr, columns='cols')
+        cols = [vector[4] for vector in vectors]
+        units = [vector[5] for vector in vectors]
+        directory = [vector[6] for vector in vectors]
+        return [(arr, cols, units, directory)]
+    MESSAGE_DICT[0xDE] = (parse_BMS_balancing_status, "BMS_balancing_status")
+
+class BMS_coulomb_counts(data):
+    def parse(data, id = None, time = None):
+        msg_id = 0xE2
+        if id is not None:
+            mask = id==msg_id
+            data = data[mask]
+            if time is not None:
+                time = time[mask]
+    
+        bitoffsets, bitmasks = get_offsets_masks(vectors)
+        arr = parse_to_np(data, vectors, bitoffsets, bitmasks, time = time)
+
+        #df = pd.DataFrame(arr, columns='cols')
+        cols = [vector[4] for vector in vectors]
+        units = [vector[5] for vector in vectors]
+        directory = [vector[6] for vector in vectors]
+        return [(arr, cols, units, directory)]
+    MESSAGE_DICT[0xE2] = (parse_BMS_coulomb_counts, "BMS_coulomb_counts") 
+
+class BMS_detailed_temperatures:
+    def parse(data, id = None, time = None):
+        msg_id = 0xDA
+        if id is not None:
+            mask = id==msg_id
+            data = data[mask]
+            if time is not None:
+                time = time[mask]
+    
+        vectors = [
+            (4 , -1, False, lambda x: x    , "ic"    , "", "BMS.detailed_temps.ic"),
+            (4 , -1, False, lambda x: x    , "group" , "", "BMS.detailed_temps.group"),
+            (16, -1,  True, lambda x: x/100, "temps" , "C", "BMS.detailed_temps.temps[0]"),
+            (16, -1,  True, lambda x: x/100, "temps" , "C", "BMS.detailed_temps.temps[1]"),
+            (16, -1,  True, lambda x: x/100, "temps" , "C", "BMS.detailed_temps.temps[2]"),
+        ]
+
+        bitoffsets, bitmasks = get_offsets_masks(vectors)
+        arr = parse_to_np(data, vectors, bitoffsets, bitmasks, time = time)
+
+        root_dir = "BMS.detailed_temps.ic_{ic:02}.temp_{cell}"
+
+        if time is not None:
+            ic_group_list = np.unique(arr[:,(1, 2)], axis = 0)
+            cells_list = [arr[:,(0,3,4,5)][(arr[:,(1,2)]==ic_group).all(axis=1)] for ic_group in ic_group_list]
+        else:
+            ic_group_list = np.unique(arr[:,(0, 1)], axis = 0)
+            cells_list = [arr[:,(2,3,4)][(arr[:,(0,1)]==ic_group).all(axis=1)] for ic_group in ic_group_list]
+        dirs_list = [
+            [root_dir.format(ic = int(ic_group[0]), cell = int(3*ic_group[1]+1)), \
+             root_dir.format(ic = int(ic_group[0]), cell = int(3*ic_group[1]+2)), \
+             root_dir.format(ic = int(ic_group[0]), cell = int(3*ic_group[1]+3))]
+                        for ic_group in ic_group_list]
+
+        #df = pd.DataFrame(arr, columns='cols')
+        cols = [vectors[i+2][4] for i in range(3)]
+        units = [vectors[i+2][5] for i in range(3)]
+        return [(cells_list[ic_group_index], cols, units, dirs_list[ic_group_index]) for ic_group_index in range(len(ic_group_list))]
+    MESSAGE_DICT[0xDA] = (parse_BMS_detailed_temperatures, "BMS_detailed_temperatures")
+
+class 
