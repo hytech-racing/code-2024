202dcef79c8b13a16db26682eebfdfbf597f96f7
Eric Galluzzi <ericbgalluzzi@gmail.com>
2023-09-10 14:13:27 -0400

    program library

--- Libraries/STEERING_RS422/STEERING_RS422.h
+++ Libraries/STEERING_RS422/STEERING_RS422.h
@@ -8,15 +8,17 @@
 #define DEFAULT_HARDWARE_SERIAL 5
 
 #define MAX_POSITION 8192
+#define ZERO_POSITION 8180
 
 class STEERING_RS422 {
     public: 
         STEERING_RS422();
         STEERING_RS422(uint8_t serial);
         void init(unsigned long baudrate);
-        uint16_t read_steering();
+        int16_t read_steering();
         void set_zero_position(uint16_t position) {  this->zero_position = position;};
         uint16_t get_encoder_position() const { return encoder_position;};
+        void calibrate_steering();    
     private:
         static uint16_t buf;
         HardwareSerial* _serial;
--- Libraries/STEERING_RS422/STEERING_RS_422.cpp
+++ Libraries/STEERING_RS422/STEERING_RS_422.cpp
@@ -39,8 +39,9 @@ STEERING_RS422::STEERING_RS422(uint8_t serial) {
 
 void STEERING_RS422::init(unsigned long baudrate) {
     _serial->begin(baudrate);
+    this->set_zero_position(ZERO_POSITION);
 }
-uint16_t STEERING_RS422::read_steering() {
+int16_t STEERING_RS422::read_steering() {
     uint8_t readByte[3] = {0};
     _serial->write(0x64);
 
@@ -53,15 +54,18 @@ uint16_t STEERING_RS422::read_steering() {
         readByte[0] = _serial->read(); //lower half of steering
         readByte[1] = _serial->read(); //upper half of steering
         readByte[2] = _serial->read(); //error stream 
-        encoder_position = readByte[1] << 6 | readByte[0] >> 2;
-        error = readByte[0] & 2; //if high, current data is not valid, previous data is sent
-        warning = readByte[0] & 1; //if high, some operating conditions are close to limits
+        encoder_position = readByte[0] << 6 | readByte[1] >> 2;
+        error = readByte[1] & 2; //if high, current data is not valid, previous data is sent
+        warning = readByte[1] & 1; //if high, some operating conditions are close to limits
         status = readByte[2];
-        if ((MAX_POSITION + (encoder_position - zero_position)) % MAX_POSITION <= (MAX_POSITION / 2)) {
-            steering_position = -((MAX_POSITION + (encoder_position - zero_position)) % MAX_POSITION);
-        } else {
-            steering_position = MAX_POSITION - ((MAX_POSITION + (encoder_position - zero_position)) % MAX_POSITION);
-        }
+        Serial.println(encoder_position);
+        // if ((MAX_POSITION + (encoder_position - zero_position)) % MAX_POSITION <= (MAX_POSITION / 2)) {
+        //     steering_position = -((MAX_POSITION + (encoder_position - zero_position)) % MAX_POSITION);
+        // } else {
+        //     steering_position = MAX_POSITION - ((MAX_POSITION + (encoder_position - zero_position)) % MAX_POSITION);
+        // }
+        steering_position = encoder_position - zero_position ;
+        //_serial->clear();
     }
 
     
@@ -69,4 +73,27 @@ uint16_t STEERING_RS422::read_steering() {
         
     return steering_position;    
 
+}
+
+void STEERING_RS422::calibrate_steering() {
+    //start programming sequence
+    delay(1);
+    _serial->write(0xCD);
+    delay(1);
+    _serial->write(0xEF);
+    delay(1);
+    _serial->write(0x89);
+    delay(1);
+    _serial->write(0xAB);
+    delay(1);
+    _serial->write(0x5A);
+    delay(1);
+    _serial->write(0x00);
+    delay(1);
+    _serial->write(0x00);
+    delay(1);
+    _serial->write(0x0E);
+    delay(1);
+    _serial->write(0x18);
+    delay(1);
 }
