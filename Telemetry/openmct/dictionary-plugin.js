07f0b2a6b576d35790f0018083aa903dc38f9867
BANANAPEEL202 <bbob52645@gmail.com>
2023-11-02 19:41:46 -0400

    Added subfolders for each system with telemetry in each subfolder
    This took way too long

--- Telemetry/openmct/dictionary-plugin.js
+++ Telemetry/openmct/dictionary-plugin.js
@@ -15,20 +15,38 @@ var objectProvider = {
                     type: 'folder',
                     location: 'ROOT'
                 };
-            } else {
-                var measurement = dictionary.measurements.filter(function (m) {
+            }
+
+            else if (identifier.key === "MOTOR_CONTROLLER") {
+                var subsystem = dictionary.subsystems.filter(function (m) {
                     return m.key === identifier.key;
                 })[0];
                 return {
                     identifier: identifier,
-                    name: measurement.name,
-                    type: 'dashboard.telemetry',
-                    telemetry: {
-                        values: measurement.values
-                    },
+                    name: subsystem.name,
+                    type: 'folder',
                     location: 'dashboard:HT08'
                 };
             }
+            else {
+                    subsystem = dictionary.subsystems.filter(function (m) {
+                        return m.key === identifier.key.split(".")[0];
+                    })[0];
+                    var measurement = subsystem.measurements.filter(function (m) {
+                        return m.key === identifier.key;
+                    })[0];
+                    console.log("Added" + identifier.key)
+                    return {
+                        identifier: identifier,
+                        name: measurement.name,
+                        type: 'dashboard.telemetry',
+                        telemetry: {
+                            values: measurement.values
+                        },
+                        location: 'dashboard:HT08'
+                    };
+                }
+
         });
     }
 };
@@ -36,20 +54,58 @@ var objectProvider = {
 var compositionProvider = {
     appliesTo: function (domainObject) {
         return domainObject.identifier.namespace === 'dashboard' &&
-               domainObject.type === 'folder';
+               domainObject.type === 'folder'
+                && domainObject.identifier.key === 'HT08';
     },
     load: function (domainObject) {
+
         return getDictionary()
             .then(function (dictionary) {
-                return dictionary.measurements.map(function (m) {
-                    return {
-                        namespace: 'dashboard',
-                        key: m.key
-                    };
-                });
+                return dictionary.subsystems.map(function (s) {
+                        return {
+                            namespace: 'dashboard',
+                            key: s.key
+                        }
+                    });
+
+
             });
+
     }
 };
+
+var compositionProviderMC = {
+    appliesTo: function (domainObject) {
+        return domainObject.identifier.namespace === 'dashboard' &&
+            domainObject.type === 'folder'
+            && domainObject.identifier.key === 'MOTOR_CONTROLLER';
+    },
+    load: function (domainObject) {
+        return [{namespace: 'dashboard', key: 'MOTOR_CONTROLLER.mc_rl.torque_current'},
+                {namespace: 'dashboard', key: 'MOTOR_CONTROLLER.mc_rl.magnetizing_current'}]
+
+        /* does not work for some stupid reason
+        return getDictionary()
+            .then(function (dictionary) {
+                return dictionary.subsystems.map(function (s) {
+                    if (s.key === "MOTOR_CONTROLLER") {
+                        return s.measurements.map(function (m) {
+                            console.log(m.key);
+                            return {
+                                namespace: 'dashboard',
+                                key: m.key
+                            }
+
+                        });
+                    }
+                });
+
+            });
+           */
+    }
+};
+
+
 
 var DictionaryPlugin = function (openmct) {
     return function install(openmct) {
@@ -61,6 +117,7 @@ var DictionaryPlugin = function (openmct) {
         openmct.objects.addProvider('dashboard', objectProvider);
 
         openmct.composition.addProvider(compositionProvider);
+        openmct.composition.addProvider(compositionProviderMC);
 
         openmct.types.addType('dashboard.telemetry', {
             name: 'Data Series',
