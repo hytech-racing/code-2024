d7c3a6bed2a4a9397213963d482cdbab8542a815
BANANAPEEL202 <bbob52645@gmail.com>
2023-12-23 14:05:17 -0800

    Added thermistors
    2 thermistors in MCU
    4 in SAB 1
    3 in SAB 2

---
+++ Libraries/HyTech_CAN/MCU_thermistors.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <string.h>
+#include <stdint.h>
+
+#pragma pack(push,1)
+
+// @Parseclass
+class MCU_thermistors {
+public:
+    MCU_thermistors() = default;
+    MCU_thermistors(const uint8_t buf[8]) { load(buf); }
+
+    inline void load(const uint8_t buf[])  { memcpy(this, buf, sizeof(*this)); }
+    inline void write(uint8_t buf[]) const { memcpy(buf, this, sizeof(*this)); }
+
+    // Getters
+    inline uint16_t get_thermistor1()                 const { return thermistor1; }
+    inline uint16_t get_thermistor2()                 const { return thermistor2; }
+
+    // Setters
+    inline void set_thermistor1(const uint16_t value)                       { this->thermistor1 = value; }
+    inline void set_thermistor2(const uint16_t value)                       { this->thermistor2 = value; }
+
+private:
+    uint16_t thermistor1;
+    uint16_t thermistor2;
+};
+
+#pragma pack(pop)
---
+++ Libraries/HyTech_CAN/SAB_thermistors_1.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <string.h>
+#include <stdint.h>
+
+#pragma pack(push,1)
+
+// @Parseclass
+class SAB_thermistors_1 {
+public:
+    SAB_thermistors_1() = default;
+    SAB_thermistors_1(const uint8_t buf[8]) { load(buf); }
+
+    inline void load(const uint8_t buf[])  { memcpy(this, buf, sizeof(*this)); }
+    inline void write(uint8_t buf[]) const { memcpy(buf, this, sizeof(*this)); }
+
+    // Getters
+    inline uint16_t get_thermistor3()                 const { return thermistor3; }
+    inline uint16_t get_thermistor4()                 const { return thermistor4; }
+    inline uint16_t get_thermistor5()                 const { return thermistor5; }
+    inline uint16_t get_thermistor6()                 const { return thermistor6; }
+
+    // Setters
+    inline void set_thermistor3(const uint16_t value)                       { this->thermistor3 = value; }
+    inline void set_thermistor4(const uint16_t value)                       { this->thermistor4 = value; }
+    inline void set_thermistor5(const uint16_t value)                       { this->thermistor5 = value; }
+    inline void set_thermistor6(const uint16_t value)                       { this->thermistor6 = value; }
+
+private: 
+    //no free bytes
+    uint16_t thermistor3;
+    uint16_t thermistor4;
+    uint16_t thermistor5;
+    uint16_t thermistor6;
+};
+
+#pragma pack(pop)
