5f9fd7dd1498d50fe1d04dd8e17edfa840d33c81
Eric Galluzzi <ericbgalluzzi@gmail.com>
2024-01-31 11:07:33 -0500

    instron testing code

---
+++ Utilities/test_load_cells_1-31/test_load_cells_1-31.ino
@@ -0,0 +1,38 @@
+#include <SPI.h>
+#include "Mcp320x.h"
+#include "Metro.h"
+
+
+#define SPI_CS      10       // SPI slave select
+#define ADC_VREF    3300     // 3.3V Vref
+#define ADC_CLK     100000  // SPI clock 1.6MHz
+
+MCP3204 adc(ADC_VREF, SPI_CS);
+Metro sample(20);
+SPISettings settings(ADC_CLK, MSBFIRST, SPI_MODE0);
+
+void setup() {
+  // configure PIN mode
+  pinMode(SPI_CS, OUTPUT);
+  
+  // set initial PIN state
+  digitalWrite(SPI_CS, HIGH);
+
+  // initialize serial
+  Serial.begin(115200);
+
+  // initialize SPI interface for MCP3208
+ 
+  SPI.begin();
+  SPI.beginTransaction(settings);
+  
+}
+void loop() {
+
+if (sample.check()) {
+  uint16_t raw = adc.read(MCP3204::Channel::SINGLE_2);
+  uint16_t val = adc.toAnalog(raw);
+  Serial.printf("\tTimestamp: %lu : value: %d mV\n", millis(), val);
+}
+
+}
