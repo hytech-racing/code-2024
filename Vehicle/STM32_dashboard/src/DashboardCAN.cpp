dcc3f3682405bad6e62b83e742b23261ddeca659
walkermburns <walkermburns@gmail.com>
2024-01-31 19:37:57 -0500

    pushing more changes

--- Vehicle/STM32_dashboard/src/DashboardCAN.cpp
+++ Vehicle/STM32_dashboard/src/DashboardCAN.cpp
@@ -91,199 +91,207 @@ void DashboardCAN::send_status() {
   // }
 
   if (should_send && send_timer.check()) {
-    _msg.id = DASHBOARD_STATE_CANID;
     //update button flags
-    SerialUSB.println("Message Sent");
-    _msg.len = sizeof(dashboard_status);
-    dashboard_status.write(_msg.buf);
+    // use HT_CAN pack function, passing in reference to message and length
+    // length will be set in pack function. Returns msg id.
+    _msg.id = Pack_DASHBOARD_STATE_ht_can(&dash_state, _msg.buf, &_msg.len, NULL);
     _CAN->write(_msg);
 
+    SerialUSB.println("Message Sent");
     send_timer.reset();
   }
 
 }
 
-uint32_t DashboardCAN::color_wheel_bms_glv(bool isBms) {
+/*
 
-  int r = 255;
-  int g = 255;
+If there is going to be this much LED logic, then this should
+be moved out of DashCAN, possibly to its own neopixel class
+that can handle the color logic and that inherits neopixel stuff
 
-  if(!isBms) {
-    // for glv
-    uint8_t max_voltage = 25;
-    uint8_t min_threshold = 23;
-    uint8_t mid_threshold = max_voltage - (max_voltage - min_threshold) / (float) 2;
-    uint8_t converted_voltage = (float) mcu_analog_readings.get_glv_battery_voltage() / 819 * 5.61;
-    if (converted_voltage > mid_threshold) {
-      g = 255;
-      r = map(mcu_analog_readings.get_glv_battery_voltage(), mid_threshold * 819 / 5.61, max_voltage * 819 / 5.61, 255, 0);
-    } else if (converted_voltage < mid_threshold) {
-      r = 255;
-      g = map(mcu_analog_readings.get_glv_battery_voltage(), min_threshold * 819 / 5.61, mid_threshold * 819 / 5.61, 0, 255);
-    } else {
-      g = 255;
-      r = 255;
-    }
-  } else {
-    // for bms
-    uint16_t max_voltage = 36000 / 10;
-    uint16_t min_threshold = PACK_THRESHOLD / 10;
-    uint16_t mid_threshold = max_voltage - (max_voltage - min_threshold) / (float)2;
-    uint16_t bms_voltage = bms_voltages.get_low() / 10;
-    if (bms_voltage > mid_threshold) {
-      g = 255;
-      r = map(bms_voltage, mid_threshold, max_voltage, 255, 0);
-    }
-    else if (bms_voltage < mid_threshold) {
-      r = 255;
-      g = map(bms_voltage, min_threshold, mid_threshold, 0, 255);
-    } else {
-      g = 255;
-      r = 255;
-    }
-  }   
-  // clamp r and g in the range [0,255]
-  r = (r < 0) ? 0 : ((r > 255) ? 255 : r);
-  g = (g < 0) ? 0 : ((g > 255) ? 255 : g);
-  return (r << 16) | (g << 8);
-}
+*/
 
-void DashboardCAN::mcu_status_received() {
-  // control BUZZER_CTRL
-  // digitalWrite(BUZZER_CTRL, mcu_status.get_activate_buzzer());
-  SerialUSB.print("AMS: ");
-  SerialUSB.println(mcu_status.get_bms_ok_high());
-  SerialUSB.print("IMD: ");
-  SerialUSB.println(mcu_status.get_imd_ok_high());
-  SerialUSB.print("IMD/AMS flags: ");
-  // SerialUSB.println(imd_ams_flags, BIN);  no work idk why
-  if (mcu_status.get_bms_ok_high()) {
-    if (((imd_ams_flags >> 1) & 1) == 0) {
-      // hytech_dashboard::set_neopixel(LED_LIST::AMS, LED_OFF);
-      dashboard_status.set_ams_led(static_cast<uint8_t>(LED_MODES::OFF));
-    //display_list[4] = 1;
-      imd_ams_flags |= (1 << 1);
-    }
-  } else if (((imd_ams_flags >> 1) & 1) == 1) {
-    if(mcu_status.get_imd_ok_high()) {
-    hytech_dashboard::getInstance()->set_neopixel(LED_LIST::AMS, LED_RED);
-    dashboard_status.set_ams_led(static_cast<uint8_t>(LED_MODES::RED));
-    //display_list[4] = 0;
-    }
-  }
+// uint32_t DashboardCAN::color_wheel_bms_glv(bool isBms) {
 
-  //IMD LED
-  if (mcu_status.get_imd_ok_high()) {
-    if ((imd_ams_flags & 1) == 0) {
-      hytech_dashboard::getInstance()->set_neopixel(LED_LIST::IMD, LED_OFF);
-      dashboard_status.set_imd_led(static_cast<uint8_t>(LED_MODES::OFF));
-    //display_list[3] = 1;
-      imd_ams_flags |= 1;
-    }
-  } else if ((imd_ams_flags & 1) == 1) {
-      hytech_dashboard::getInstance()->set_neopixel(LED_LIST::IMD, LED_RED);
-      dashboard_status.set_imd_led(static_cast<uint8_t>(LED_MODES::RED));
-    //display_list[3] = 0;    
-  } 
+//   int r = 255;
+//   int g = 255;
 
-  //Start LED
-  switch (mcu_status.get_state()) {
-    case MCU_STATE::STARTUP:
-      hytech_dashboard::getInstance()->set_neopixel(LED_LIST::RDY_DRIVE, LED_OFF);
-      dashboard_status.set_start_led(static_cast<uint8_t>(LED_MODES::OFF));
-      break;
-    case MCU_STATE::TRACTIVE_SYSTEM_NOT_ACTIVE:
-      hytech_dashboard::getInstance()->set_neopixel(LED_LIST::RDY_DRIVE, LED_OFF);
-      dashboard_status.set_start_led(static_cast<uint8_t>(LED_MODES::RED));
-      break;
-    case MCU_STATE::TRACTIVE_SYSTEM_ACTIVE:
-      hytech_dashboard::getInstance()->set_neopixel(LED_LIST::RDY_DRIVE, LED_ON_GREEN);
-      dashboard_status.set_start_led(static_cast<uint8_t>(LED_MODES::YELLOW));
-      break;
-    case MCU_STATE::ENABLING_INVERTER:
-    case MCU_STATE::WAITING_READY_TO_DRIVE_SOUND:
-    case MCU_STATE::READY_TO_DRIVE:
-      hytech_dashboard::getInstance()->set_neopixel(LED_LIST::RDY_DRIVE, LED_BLUE);
-      dashboard_status.set_start_led(static_cast<uint8_t>(LED_MODES::ON));
-      break;
-    default:
-      hytech_dashboard::getInstance()->set_neopixel(LED_LIST::RDY_DRIVE, LED_OFF);
-      dashboard_status.set_start_led(static_cast<uint8_t>(LED_MODES::OFF));
-      break;
-  }
+//   if(!isBms) {
+//     // for glv
+//     uint8_t max_voltage = 25;
+//     uint8_t min_threshold = 23;
+//     uint8_t mid_threshold = max_voltage - (max_voltage - min_threshold) / (float) 2;
+//     uint8_t converted_voltage = (float) mcu_analog_readings.get_glv_battery_voltage() / 819 * 5.61;
+//     if (converted_voltage > mid_threshold) {
+//       g = 255;
+//       r = map(mcu_analog_readings.get_glv_battery_voltage(), mid_threshold * 819 / 5.61, max_voltage * 819 / 5.61, 255, 0);
+//     } else if (converted_voltage < mid_threshold) {
+//       r = 255;
+//       g = map(mcu_analog_readings.get_glv_battery_voltage(), min_threshold * 819 / 5.61, mid_threshold * 819 / 5.61, 0, 255);
+//     } else {
+//       g = 255;
+//       r = 255;
+//     }
+//   } else {
+//     // for bms
+//     uint16_t max_voltage = 36000 / 10;
+//     uint16_t min_threshold = PACK_THRESHOLD / 10;
+//     uint16_t mid_threshold = max_voltage - (max_voltage - min_threshold) / (float)2;
+//     uint16_t bms_voltage = bms_voltages.get_low() / 10;
+//     if (bms_voltage > mid_threshold) {
+//       g = 255;
+//       r = map(bms_voltage, mid_threshold, max_voltage, 255, 0);
+//     }
+//     else if (bms_voltage < mid_threshold) {
+//       r = 255;
+//       g = map(bms_voltage, min_threshold, mid_threshold, 0, 255);
+//     } else {
+//       g = 255;
+//       r = 255;
+//     }
+//   }   
+//   // clamp r and g in the range [0,255]
+//   r = (r < 0) ? 0 : ((r > 255) ? 255 : r);
+//   g = (g < 0) ? 0 : ((g > 255) ? 255 : g);
+//   return (r << 16) | (g << 8);
+// }
 
-  //Critical Charge LED // handled by reading BMS lowest value
+// void DashboardCAN::mcu_status_received() {
+//   // control BUZZER_CTRL
+//   // digitalWrite(BUZZER_CTRL, mcu_status.get_activate_buzzer());
+//   SerialUSB.print("AMS: ");
+//   SerialUSB.println(mcu_status.get_bms_ok_high());
+//   SerialUSB.print("IMD: ");
+//   SerialUSB.println(mcu_status.get_imd_ok_high());
+//   SerialUSB.print("IMD/AMS flags: ");
+//   // SerialUSB.println(imd_ams_flags, BIN);  no work idk why
+//   if (mcu_status.get_bms_ok_high()) {
+//     if (((imd_ams_flags >> 1) & 1) == 0) {
+//       // hytech_dashboard::set_neopixel(LED_LIST::AMS, LED_OFF);
+//       dashboard_status.set_ams_led(static_cast<uint8_t>(LED_MODES::OFF));
+//     //display_list[4] = 1;
+//       imd_ams_flags |= (1 << 1);
+//     }
+//   } else if (((imd_ams_flags >> 1) & 1) == 1) {
+//     if(mcu_status.get_imd_ok_high()) {
+//     hytech_dashboard::getInstance()->set_neopixel(LED_LIST::AMS, LED_RED);
+//     dashboard_status.set_ams_led(static_cast<uint8_t>(LED_MODES::RED));
+//     //display_list[4] = 0;
+//     }
+//   }
 
-  switch (mcu_status.get_pack_charge_critical()) {
-    case 1: // GREEN, OK
-      dashboard_status.set_crit_charge_led(static_cast<uint8_t>(LED_MODES::ON));
-      break;
-    case 2: // YELLOW, WARNING
-      dashboard_status.set_crit_charge_led(static_cast<uint8_t>(LED_MODES::YELLOW));
-      break;
-    case 3: // RED, CRITICAL
-      dashboard_status.set_crit_charge_led(static_cast<uint8_t>(LED_MODES::RED));
-      break;
-    default:
-      dashboard_status.set_crit_charge_led(static_cast<uint8_t>(LED_MODES::OFF));
-      break;
-  }
+//   //IMD LED
+//   if (mcu_status.get_imd_ok_high()) {
+//     if ((imd_ams_flags & 1) == 0) {
+//       hytech_dashboard::getInstance()->set_neopixel(LED_LIST::IMD, LED_OFF);
+//       dashboard_status.set_imd_led(static_cast<uint8_t>(LED_MODES::OFF));
+//     //display_list[3] = 1;
+//       imd_ams_flags |= 1;
+//     }
+//   } else if ((imd_ams_flags & 1) == 1) {
+//       hytech_dashboard::getInstance()->set_neopixel(LED_LIST::IMD, LED_RED);
+//       dashboard_status.set_imd_led(static_cast<uint8_t>(LED_MODES::RED));
+//     //display_list[3] = 0;    
+//   } 
 
-  // Mode LED
-  switch (mcu_status.get_torque_mode()) {
-    case 1:
-      hytech_dashboard::getInstance()->set_neopixel(LED_LIST::TORQUE_MODE, LED_OFF);
-      dashboard_status.set_mode_led(static_cast<uint8_t>(LED_MODES::OFF));
-      break;
-    case 2:
-      hytech_dashboard::getInstance()->set_neopixel(LED_LIST::TORQUE_MODE, LED_YELLOW);
-      dashboard_status.set_mode_led(static_cast<uint8_t>(LED_MODES::YELLOW));
-      break;
-    case 3:
-      hytech_dashboard::getInstance()->set_neopixel(LED_LIST::TORQUE_MODE, LED_ON_GREEN);
-      dashboard_status.set_mode_led(static_cast<uint8_t>(LED_MODES::ON));
-      break;
-    default:
-      //led_mode.setMode(LED_MODES::OFF);
-      //dashboard_status.set_mode_led(static_cast<uint8_t>(LED_MODES::OFF));
-      break;
-  }
+//   //Start LED
+//   switch (mcu_status.get_state()) {
+//     case MCU_STATE::STARTUP:
+//       hytech_dashboard::getInstance()->set_neopixel(LED_LIST::RDY_DRIVE, LED_OFF);
+//       dashboard_status.set_start_led(static_cast<uint8_t>(LED_MODES::OFF));
+//       break;
+//     case MCU_STATE::TRACTIVE_SYSTEM_NOT_ACTIVE:
+//       hytech_dashboard::getInstance()->set_neopixel(LED_LIST::RDY_DRIVE, LED_OFF);
+//       dashboard_status.set_start_led(static_cast<uint8_t>(LED_MODES::RED));
+//       break;
+//     case MCU_STATE::TRACTIVE_SYSTEM_ACTIVE:
+//       hytech_dashboard::getInstance()->set_neopixel(LED_LIST::RDY_DRIVE, LED_ON_GREEN);
+//       dashboard_status.set_start_led(static_cast<uint8_t>(LED_MODES::YELLOW));
+//       break;
+//     case MCU_STATE::ENABLING_INVERTER:
+//     case MCU_STATE::WAITING_READY_TO_DRIVE_SOUND:
+//     case MCU_STATE::READY_TO_DRIVE:
+//       hytech_dashboard::getInstance()->set_neopixel(LED_LIST::RDY_DRIVE, LED_BLUE);
+//       dashboard_status.set_start_led(static_cast<uint8_t>(LED_MODES::ON));
+//       break;
+//     default:
+//       hytech_dashboard::getInstance()->set_neopixel(LED_LIST::RDY_DRIVE, LED_OFF);
+//       dashboard_status.set_start_led(static_cast<uint8_t>(LED_MODES::OFF));
+//       break;
+//   }
 
-  //Mechanical Braking LED
-  if (!mcu_status.get_mech_brake_active()) {
-    hytech_dashboard::getInstance()->set_neopixel(LED_LIST::BRAKE_ENGAGE, LED_OFF);
-    dashboard_status.set_mech_brake_led(static_cast<uint8_t>(LED_MODES::OFF));
-  } else {
-    hytech_dashboard::getInstance()->set_neopixel(LED_LIST::BRAKE_ENGAGE, LED_BLUE);
-    dashboard_status.set_mech_brake_led(static_cast<uint8_t>(LED_MODES::ON));
-  }
+//   //Critical Charge LED // handled by reading BMS lowest value
 
-  if (!mcu_status.get_launch_ctrl_active()) {
-    hytech_dashboard::getInstance()->set_neopixel(LED_LIST::LAUNCH_CTRL, LED_OFF);
-    dashboard_status.set_launch_control_led(static_cast<uint8_t>(LED_MODES::OFF));
-  } else {
-    hytech_dashboard::getInstance()->set_neopixel(LED_LIST::LAUNCH_CTRL, LED_ON_GREEN);
-    dashboard_status.set_launch_control_led(static_cast<uint8_t>(LED_MODES::ON));
-  }
+//   switch (mcu_status.get_pack_charge_critical()) {
+//     case 1: // GREEN, OK
+//       dashboard_status.set_crit_charge_led(static_cast<uint8_t>(LED_MODES::ON));
+//       break;
+//     case 2: // YELLOW, WARNING
+//       dashboard_status.set_crit_charge_led(static_cast<uint8_t>(LED_MODES::YELLOW));
+//       break;
+//     case 3: // RED, CRITICAL
+//       dashboard_status.set_crit_charge_led(static_cast<uint8_t>(LED_MODES::RED));
+//       break;
+//     default:
+//       dashboard_status.set_crit_charge_led(static_cast<uint8_t>(LED_MODES::OFF));
+//       break;
+//   }
 
-  if (!mcu_status.get_inverters_error()){
-    hytech_dashboard::getInstance()->set_neopixel(LED_LIST::MC_ERR, LED_ON_GREEN);
-    dashboard_status.set_mc_error_led(0);
-  } else {
-    hytech_dashboard::getInstance()->set_neopixel(LED_LIST::MC_ERR, LED_YELLOW);
-    dashboard_status.set_mc_error_led(1);
-  }
-}
+//   // Mode LED
+//   switch (mcu_status.get_torque_mode()) {
+//     case 1:
+//       hytech_dashboard::getInstance()->set_neopixel(LED_LIST::TORQUE_MODE, LED_OFF);
+//       dashboard_status.set_mode_led(static_cast<uint8_t>(LED_MODES::OFF));
+//       break;
+//     case 2:
+//       hytech_dashboard::getInstance()->set_neopixel(LED_LIST::TORQUE_MODE, LED_YELLOW);
+//       dashboard_status.set_mode_led(static_cast<uint8_t>(LED_MODES::YELLOW));
+//       break;
+//     case 3:
+//       hytech_dashboard::getInstance()->set_neopixel(LED_LIST::TORQUE_MODE, LED_ON_GREEN);
+//       dashboard_status.set_mode_led(static_cast<uint8_t>(LED_MODES::ON));
+//       break;
+//     default:
+//       //led_mode.setMode(LED_MODES::OFF);
+//       //dashboard_status.set_mode_led(static_cast<uint8_t>(LED_MODES::OFF));
+//       break;
+//   }
 
-void DashboardCAN::mcu_analog_readings_received() {
-  if (mcu_analog_readings.get_glv_battery_voltage() < GLV_THRESHOLD) {
-    dashboard_status.set_glv_led(static_cast<uint8_t>(LED_MODES::RED));
-  } else {
-    dashboard_status.set_glv_led(static_cast<uint8_t>(LED_MODES::ON));
-  }
-  hytech_dashboard::getInstance()->set_neopixel(LED_LIST::GLV, color_wheel_bms_glv(false));
-}
+//   //Mechanical Braking LED
+//   if (!mcu_status.get_mech_brake_active()) {
+//     hytech_dashboard::getInstance()->set_neopixel(LED_LIST::BRAKE_ENGAGE, LED_OFF);
+//     dashboard_status.set_mech_brake_led(static_cast<uint8_t>(LED_MODES::OFF));
+//   } else {
+//     hytech_dashboard::getInstance()->set_neopixel(LED_LIST::BRAKE_ENGAGE, LED_BLUE);
+//     dashboard_status.set_mech_brake_led(static_cast<uint8_t>(LED_MODES::ON));
+//   }
 
-void DashboardCAN::bms_voltages_received() {
-  hytech_dashboard::getInstance()->set_neopixel(LED_LIST::CRIT_CHARGE, color_wheel_bms_glv(true));
-}
+//   if (!mcu_status.get_launch_ctrl_active()) {
+//     hytech_dashboard::getInstance()->set_neopixel(LED_LIST::LAUNCH_CTRL, LED_OFF);
+//     dashboard_status.set_launch_control_led(static_cast<uint8_t>(LED_MODES::OFF));
+//   } else {
+//     hytech_dashboard::getInstance()->set_neopixel(LED_LIST::LAUNCH_CTRL, LED_ON_GREEN);
+//     dashboard_status.set_launch_control_led(static_cast<uint8_t>(LED_MODES::ON));
+//   }
+
+//   if (!mcu_status.get_inverters_error()){
+//     hytech_dashboard::getInstance()->set_neopixel(LED_LIST::MC_ERR, LED_ON_GREEN);
+//     dashboard_status.set_mc_error_led(0);
+//   } else {
+//     hytech_dashboard::getInstance()->set_neopixel(LED_LIST::MC_ERR, LED_YELLOW);
+//     dashboard_status.set_mc_error_led(1);
+//   }
+// }
+
+// void DashboardCAN::mcu_analog_readings_received() {
+//   if (mcu_analog_readings.get_glv_battery_voltage() < GLV_THRESHOLD) {
+//     dashboard_status.set_glv_led(static_cast<uint8_t>(LED_MODES::RED));
+//   } else {
+//     dashboard_status.set_glv_led(static_cast<uint8_t>(LED_MODES::ON));
+//   }
+//   hytech_dashboard::getInstance()->set_neopixel(LED_LIST::GLV, color_wheel_bms_glv(false));
+// }
+
+// void DashboardCAN::bms_voltages_received() {
+//   hytech_dashboard::getInstance()->set_neopixel(LED_LIST::CRIT_CHARGE, color_wheel_bms_glv(true));
+// }
--- Vehicle/STM32_dashboard/src/Dashboard_Controls.cpp
+++ Vehicle/STM32_dashboard/src/Dashboard_Controls.cpp
@@ -26,14 +26,11 @@ void Dashboard_Controls::update(DashboardCAN* CAN) {
   if (btn_mark.isPressed())
     new_state.start_button = true;
 
-  if (btn_mode.isPressed())
+  if (btn_torque_mode.isPressed())
     new_state.mark_button = true;
 
   if (btn_mc_cycle.isPressed())
     new_state.motor_controller_cycle_button = true;
-
-  // if (btn_launch_ctrl.isPressed())
-  //   new_state.launch_ctrl_button = true;
 
   if (btn_torque_mode.isPressed())
     new_state.torque_mode_button = true;
@@ -41,11 +38,11 @@ void Dashboard_Controls::update(DashboardCAN* CAN) {
   if (btn_led_dimmer.isPressed())
     new_state.led_dimmer_button = true;
 
-  // if (btn_left_shifter.isPressed())
-  //   new_state.left_shifter_button = true;
+  if (btn_left_shifter.isPressed())
+    new_state.left_shifter_button = true;
 
-  // if (btn_right_shifter.isPressed())
-  //   new_state.right_shifter_button = true;
+  if (btn_right_shifter.isPressed())
+    new_state.right_shifter_button = true;
 
   if (buzzer_active)
     new_state.drive_buzzer = true;
@@ -53,40 +50,42 @@ void Dashboard_Controls::update(DashboardCAN* CAN) {
   //copy new state over previous dash state
   memcpy(&CAN->dash_state, &new_state, sizeof(DASHBOARD_STATE_t));
 
-  // this sets the button to be high: it is set low in send can
-  hytech_dashboard::getInstance()->set_neopixel(1, LED_OFF);
-  if (btn_safe_ctrl.isPressed())  {
-    CAN->dashboard_status.toggle_mode_btn();
-    if(!previousState) {
-      // SerialUSB.println("state incrementing");
-      // SerialUSB.println("Previous state false");
-      hytech_dashboard::getInstance()->increment_state();
-      previousState = true;
-    }
-    SerialUSB.println("BTN SAFE CTRL PRESSED");
-    hytech_dashboard::getInstance()->set_neopixel(1, LED_YELLOW);
-  } else {
-    // SerialUSB.println("Previous state reset");
-    previousState = false;
-  }
-  if (btn_mc_cycle.isPressed())    {
-    CAN->dashboard_status.toggle_mc_cycle_btn();
-    SerialUSB.println("BTN MC CYCLE PRESSED");
-    hytech_dashboard::getInstance()->set_neopixel(1, LED_BLUE);
-  }
-  if (btn_torque_mode.isPressed()) {
-    CAN->dashboard_status.toggle_torque_mode_btn();
-    SerialUSB.println("BTN TORQUE MODE PRESSED");
-    hytech_dashboard::getInstance()->set_neopixel(1, LED_RED);
-  }
-  if (btn_led_dimmer.isPressed())  {
-    CAN->dashboard_status.toggle_led_dimmer_btn();
-    SerialUSB.println("BTN LED DIMMER PRESSED");
-    hytech_dashboard::getInstance()->set_neopixel(1, LED_ON_GREEN);
-  }
-  if(btn_restart_timer.isPressed()) {
-    SerialUSB.println("BTN RESTART TIMER PRESSED");
-    hytech_dashboard::getInstance()->restart_current_timer();
-  }
-  CAN->dashboard_status.set_start_btn(btn_start.isPressed());
+  // old code
+
+  // // this sets the button to be high: it is set low in send can
+  // hytech_dashboard::getInstance()->set_neopixel(1, LED_OFF);
+  // if (btn_safe_ctrl.isPressed())  {
+  //   CAN->dashboard_status.toggle_mode_btn();
+  //   if(!previousState) {
+  //     // SerialUSB.println("state incrementing");
+  //     // SerialUSB.println("Previous state false");
+  //     hytech_dashboard::getInstance()->increment_state();
+  //     previousState = true;
+  //   }
+  //   SerialUSB.println("BTN SAFE CTRL PRESSED");
+  //   hytech_dashboard::getInstance()->set_neopixel(1, LED_YELLOW);
+  // } else {
+  //   // SerialUSB.println("Previous state reset");
+  //   previousState = false;
+  // }
+  // if (btn_mc_cycle.isPressed())    {
+  //   CAN->dashboard_status.toggle_mc_cycle_btn();
+  //   SerialUSB.println("BTN MC CYCLE PRESSED");
+  //   hytech_dashboard::getInstance()->set_neopixel(1, LED_BLUE);
+  // }
+  // if (btn_torque_mode.isPressed()) {
+  //   CAN->dashboard_status.toggle_torque_mode_btn();
+  //   SerialUSB.println("BTN TORQUE MODE PRESSED");
+  //   hytech_dashboard::getInstance()->set_neopixel(1, LED_RED);
+  // }
+  // if (btn_led_dimmer.isPressed())  {
+  //   CAN->dashboard_status.toggle_led_dimmer_btn();
+  //   SerialUSB.println("BTN LED DIMMER PRESSED");
+  //   hytech_dashboard::getInstance()->set_neopixel(1, LED_ON_GREEN);
+  // }
+  // if(btn_restart_timer.isPressed()) {
+  //   SerialUSB.println("BTN RESTART TIMER PRESSED");
+  //   hytech_dashboard::getInstance()->restart_current_timer();
+  // }
+  // CAN->dashboard_status.set_start_btn(btn_start.isPressed());
 }
