4ecedbeee50b873190cd89457dc705ffcc6bb7b2
Siddharth Lohani <me@siddharthlohani.dev>
2024-02-08 21:31:57 -0500

    Update quotes.h

--- Vehicle/STM32_dashboard/src/quotes.h
+++ Vehicle/STM32_dashboard/src/quotes.h
@@ -3,8 +3,35 @@
 #define NUMBER_OF_MESSAGES 10
 
 String greetings[NUMBER_OF_MESSAGES] = {
-    "hello",
-    "stepbro, im stuck in the washer",
-    "what's up"
-};
+    "Yaaay!",
+    "Sponsored by ChatGPT",
+    "Currently on fire",
+    "Made with titanium",
+    "Ur mom",
+    "Ice Spice",
+    "Days since last IRacing incident: 0",
+    "Current iRating: 420",
+    "Wat?",
+    "Electrically challenged",
+    "Driver skill issue",
+    "Performance anxiety",
+    "Contains 20 miles of wire",
+    "Our mascot is literally an insect",
+    "911 on speed dial",
+    "Turing complete!"
+    "Bees, bees, bees, bees!",
+    "Internet enabled!",
+    "Supercalifragilisticexpialidocious",
+    "sqrt(-1) love you!",
+    "Bringing home the bacon!",
+    "Multithreaded!",
+    "Buzzy Bees!",
+    "Afraid of the big, black bat!",
+    "Got your nose!",
+    "Casual gaming!",
+    "Undefeated!",
+    "12345 is a bad password!",
+    "May contain nuts!",
+    "Should not be played while driving."
+}
 
