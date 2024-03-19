694da40759595e6f142035185827f019904dfea2
CL16gtgh <ryang359@gatech.edu>
2024-03-19 14:16:35 -0400

    weird code in order to take advantage of the software library

--- Utilities/vectornav_test/src/main.cpp
+++ Utilities/vectornav_test/src/main.cpp
@@ -3,6 +3,7 @@
 */
 #include <Arduino.h>
 
+#include "Metro.h"
 #include "types.h"
 
 #define IMU_RS232_SPEED 115200
@@ -14,12 +15,16 @@
 	if (result == NULL) \
 		return;
 
+Metro timer_vectornav_read = Metro(25);  // default 40Hz
+
 // Function declarations
 void queryModelNumber();
 void parseModelNumber(char *productName);
 void queryYawPitchRoll();
 void queryYawPitchRollMagneticAccelerationAndAngularRates();
 void queryAsyncDataOutputFrequency();
+void readDefaultAsciiOutput_INS_LLA();
+void readDefaultAsciiOutput_INS_LLA_40Hz();
 
 // Test utilities
 void parseAsyncDataOutputFrequency(uint32_t* adof, char *_data);
@@ -46,8 +51,8 @@ void setup() {
 
 void loop() {
   // Test communications
-  Serial.println("Ask Vector Nav something ^^");
-  Serial.println("Let's get raw reponse first");
+  // Serial.println("Ask Vector Nav something ^^");
+  // Serial.println("Let's get raw reponse first");
   // Serial.print("Model number: ");
   // queryModelNumber();
   // Serial.print("Yaw Pitch Roll: ");
@@ -56,6 +61,9 @@ void loop() {
   // queryYawPitchRollMagneticAccelerationAndAngularRates();
   // Serial.print("Async rate: ");
   // queryAsyncDataOutputFrequency();
+  // Serial.print("Default async ascii output: ");
+  // readDefaultAsciiOutput_INS_LLA();
+  readDefaultAsciiOutput_INS_LLA_40Hz();
 
   // char receiveBuffer[DEFAULT_READ_BUFFER_SIZE] = "$VNRRG,07,40*5C";
 
@@ -68,20 +76,20 @@ void loop() {
 
   // Attempt to received async data periodically assuming Vecotr Nav is sending
 
-  char data = '\0';
+  // char data = '\0';
 
-  if (Serial2.available()) {
-    data = Serial2.read();
-    counter++;
-    // Serial.print("Last byte arrived: ");
-    // Serial.println(data);
-    Serial.println("Async data received ... Maybe?");
-  }
-  Serial.printf("Number of bytes read: %d\n", counter);
-  Serial.print("Last byte arrived: ");
-  Serial.println(data);
+  // if (Serial2.available()) {
+  //   data = Serial2.read();
+  //   counter++;
+  //   // Serial.print("Last byte arrived: ");
+  //   // Serial.println(data);
+  //   Serial.println("Async data received ... Maybe?");
+  // }
+  // Serial.printf("Number of bytes read: %d\n", counter);
+  // Serial.print("Last byte arrived: ");
+  // Serial.println(data);
 
-  Serial.println();
+  // Serial.println();
   
 }
 
@@ -204,86 +212,200 @@ char* vnstrtok(char* str, size_t& startIndex)
 }
 
 // Compose binary output command
-void generateBinaryOutputConfigCommand(uint8_t binaryOutputNumber, char *toSend)
-	{
-		// char toSend[256];
-		// Packet response;
+void generateBinaryOutputConfigCommand(uint8_t binaryOutputNumber, char *toSend) {
+  // char toSend[256];
+  // Packet response;
 
-		// First determine which groups are present.
-		uint16_t groups = 0;
-		// if (fields.commonField)
-			groups |= 0x0001;
-		// if (fields.timeField)
-			// groups |= 0x0002;
-		// if (fields.imuField)
-			// groups |= 0x0004;
-		// if (fields.gpsField)
-			// groups |= 0x0008;
-		// if (fields.attitudeField)
-			// groups |= 0x0010;
-		// if (fields.insField)
-			groups |= 0x0020;
-    // if(fields.gps2Field)
-      // groups |= 0x0040;
+  // First determine which groups are present.
+  uint16_t groups = 0;
+  // if (fields.commonField)
+    groups |= 0x0001;
+  // if (fields.timeField)
+    // groups |= 0x0002;
+  // if (fields.imuField)
+    // groups |= 0x0004;
+  // if (fields.gpsField)
+    // groups |= 0x0008;
+  // if (fields.attitudeField)
+    // groups |= 0x0010;
+  // if (fields.insField)
+    groups |= 0x0020;
+  // if(fields.gps2Field)
+    // groups |= 0x0040;
 
-		#if VN_HAVE_SECURE_CRT
-		int length = sprintf_s(toSend, sizeof(toSend), "$VNWRG,%u,%u,%u,%X", 74 + binaryOutputNumber, fields.asyncMode, fields.rateDivisor, groups);
-		#else
-		int length = sprintf(toSend, "$VNWRG,%u,%u,%u,%X", 74 + binaryOutputNumber, ASYNCMODE_PORT1, 16, groups); // serial1, 800/16=50Hz, 
-		#endif
+  #if VN_HAVE_SECURE_CRT
+  int length = sprintf_s(toSend, sizeof(toSend), "$VNWRG,%u,%u,%u,%X", 74 + binaryOutputNumber, fields.asyncMode, fields.rateDivisor, groups);
+  #else
+  int length = sprintf(toSend, "$VNWRG,%u,%u,%u,%X", 74 + binaryOutputNumber, vn::protocol::uart::ASYNCMODE_PORT1, 16, groups); // serial1, 800/16=50Hz, 
+  #endif
 
-		// if (fields.commonField)
-			#if VN_HAVE_SECURE_CRT
-			length += sprintf_s(toSend + length, sizeof(toSend) - length, ",%X", fields.commonField);
-			#else
-			length += sprintf(toSend + length, ",%X", COMMONGROUP_TIMESTARTUP | COMMONGROUP_TIMEGPS | COMMONGROUP_TIMESYNCIN | COMMONGROUP_YAWPITCHROLL | COMMONGROUP_QUATERNION | COMMONGROUP_ANGULARRATE | COMMONGROUP_POSITION | COMMONGROUP_VELOCITY | COMMONGROUP_ACCEL | COMMONGROUP_IMU | COMMONGROUP_MAGPRES | COMMONGROUP_DELTATHETA | COMMONGROUP_INSSTATUS | COMMONGROUP_SYNCINCNT);
-			#endif
-		// if (fields.timeField)
-		// 	#if VN_HAVE_SECURE_CRT
-		// 	length += sprintf_s(toSend + length, sizeof(toSend) - length, ",%X", fields.timeField);
-		// 	#else
-		// 	length += sprintf(toSend + length, ",%X", fields.timeField);
-		// 	#endif
-		// if (fields.imuField)
-		// 	#if VN_HAVE_SECURE_CRT
-		// 	length += sprintf_s(toSend + length, sizeof(toSend) - length, ",%X", fields.imuField);
-		// 	#else
-		// 	length += sprintf(toSend + length, ",%X", fields.imuField);
-		// 	#endif
-		// if (fields.gpsField)
-		// 	#if VN_HAVE_SECURE_CRT
-		// 	length += sprintf_s(toSend + length, sizeof(toSend) - length, ",%X", fields.gpsField);
-		// 	#else
-		// 	length += sprintf(toSend + length, ",%X", fields.gpsField);
-		// 	#endif
-		// if (fields.attitudeField)
-		// 	#if VN_HAVE_SECURE_CRT
-		// 	length += sprintf_s(toSend + length, sizeof(toSend) - length, ",%X", fields.attitudeField);
-		// 	#else
-		// 	length += sprintf(toSend + length, ",%X", fields.attitudeField);
-		// 	#endif
-		// if (fields.insField)
-			#if VN_HAVE_SECURE_CRT
-			length += sprintf_s(toSend + length, sizeof(toSend) - length, ",%X", fields.insField);
-			#else
-			length += sprintf(toSend + length, ",%X", INSGROUP_VELBODY);
-			#endif
-    // if(fields.gps2Field)
-    //   #if VN_HAVE_SECURE_CRT
-    //   length += sprintf_s(toSend + length, sizeof(toSend) - length, ",%X", fields.gps2Field);
-    //   #else
-    //   length += sprintf(toSend + length, ",%X", fields.gps2Field);
-    //   #endif
+  // if (fields.commonField)
+    #if VN_HAVE_SECURE_CRT
+    length += sprintf_s(toSend + length, sizeof(toSend) - length, ",%X", fields.commonField);
+    #else
+    length += sprintf(toSend + length, ",%X", vn::protocol::uart::COMMONGROUP_TIMESTARTUP | 
+                                              vn::protocol::uart::COMMONGROUP_TIMEGPS | 
+                                              vn::protocol::uart::COMMONGROUP_TIMESYNCIN | 
+                                              vn::protocol::uart::COMMONGROUP_YAWPITCHROLL | 
+                                              vn::protocol::uart::COMMONGROUP_QUATERNION | 
+                                              vn::protocol::uart::COMMONGROUP_ANGULARRATE | 
+                                              vn::protocol::uart::COMMONGROUP_POSITION | 
+                                              vn::protocol::uart::COMMONGROUP_VELOCITY | 
+                                              vn::protocol::uart::COMMONGROUP_ACCEL | 
+                                              vn::protocol::uart::COMMONGROUP_IMU | 
+                                              vn::protocol::uart::COMMONGROUP_MAGPRES | 
+                                              vn::protocol::uart::COMMONGROUP_DELTATHETA | 
+                                              vn::protocol::uart::COMMONGROUP_INSSTATUS | 
+                                              vn::protocol::uart::COMMONGROUP_SYNCINCNT);
+    #endif
+  // if (fields.timeField)
+  // 	#if VN_HAVE_SECURE_CRT
+  // 	length += sprintf_s(toSend + length, sizeof(toSend) - length, ",%X", fields.timeField);
+  // 	#else
+  // 	length += sprintf(toSend + length, ",%X", fields.timeField);
+  // 	#endif
+  // if (fields.imuField)
+  // 	#if VN_HAVE_SECURE_CRT
+  // 	length += sprintf_s(toSend + length, sizeof(toSend) - length, ",%X", fields.imuField);
+  // 	#else
+  // 	length += sprintf(toSend + length, ",%X", fields.imuField);
+  // 	#endif
+  // if (fields.gpsField)
+  // 	#if VN_HAVE_SECURE_CRT
+  // 	length += sprintf_s(toSend + length, sizeof(toSend) - length, ",%X", fields.gpsField);
+  // 	#else
+  // 	length += sprintf(toSend + length, ",%X", fields.gpsField);
+  // 	#endif
+  // if (fields.attitudeField)
+  // 	#if VN_HAVE_SECURE_CRT
+  // 	length += sprintf_s(toSend + length, sizeof(toSend) - length, ",%X", fields.attitudeField);
+  // 	#else
+  // 	length += sprintf(toSend + length, ",%X", fields.attitudeField);
+  // 	#endif
+  // if (fields.insField)
+    #if VN_HAVE_SECURE_CRT
+    length += sprintf_s(toSend + length, sizeof(toSend) - length, ",%X", fields.insField);
+    #else
+    length += sprintf(toSend + length, ",%X", vn::protocol::uart::INSGROUP_VELBODY);
+    #endif
+  // if(fields.gps2Field)
+  //   #if VN_HAVE_SECURE_CRT
+  //   length += sprintf_s(toSend + length, sizeof(toSend) - length, ",%X", fields.gps2Field);
+  //   #else
+  //   length += sprintf(toSend + length, ",%X", fields.gps2Field);
+  //   #endif
 
-		#if VN_HAVE_SECURE_CRT
-		length += sprintf_s(toSend + length, sizeof(toSend) - length, "*");
-		#else
-		length += sprintf(toSend + length, "*");
-		#endif
+  #if VN_HAVE_SECURE_CRT
+  length += sprintf_s(toSend + length, sizeof(toSend) - length, "*");
+  #else
+  length += sprintf(toSend + length, "*");
+  #endif
 
-    length += sprintf(toSend + length, "XX\r\n");
+  length += sprintf(toSend + length, "XX\r\n");
 
-		// transaction(toSend, length, waitForReply, &response);
-	}
+  // transaction(toSend, length, waitForReply, &response);
+}
+
+void readDefaultAsciiOutput_INS_LLA() {
+
+  int index = 0;
+  char receiveBuffer[DEFAULT_READ_BUFFER_SIZE] = {'\0'};
+  bool start_countdown = false;
+  int count_down = 2;
+
+  while (1) {
+    if (Serial2.available()) {
+      char data = Serial2.read();
+
+      if (data == '$') {
+        receiveBuffer[index++] = data;
+
+        while (1) {
+
+          if (Serial2.available()) {
+            data = Serial2.read();
+            receiveBuffer[index++] = data;
+
+            if (start_countdown) {
+              count_down--;
+            }
+          }
+
+          if (data == '*') {
+            start_countdown = true;
+          }
+
+          if (count_down <= 0) {
+            break;
+          }
+
+        }
+
+        break;
+
+      }
+
+    }
+  }
+
+  Serial.println(receiveBuffer);
+}
+
+void readDefaultAsciiOutput_INS_LLA_40Hz() {
+
+  if (timer_vectornav_read.check()) {
+
+  Serial.println("Ask Vector Nav something ^^");
+  Serial.println("Let's get raw reponse first");
+  Serial.print("Default async ascii output: ");
+
+  int index = 0;
+  char receiveBuffer[DEFAULT_READ_BUFFER_SIZE] = {'\0'};
+  bool start_countdown = false;
+  int count_down = 2;
+
+  // while (1) {
+    while (Serial2.available()) {
+      char data = Serial2.read();
+
+      if (data == '$') {
+        receiveBuffer[index++] = data;
+
+        while (count_down > 0) {
+
+          if (Serial2.available()) {
+            data = Serial2.read();
+            receiveBuffer[index++] = data;
+
+            if (start_countdown) {
+              count_down--;
+            }
+          }
+
+          if (data == '*') {
+            start_countdown = true;
+          }
+
+          // if (count_down <= 0) {
+          //   break;
+          // }
+
+        }
+
+        break;
+
+      }
+
+    }
+  // }
+
+  Serial.println(receiveBuffer);
+
+  Serial.println();
+
+  } // end of timer.check()
+}
+
+
 
 
